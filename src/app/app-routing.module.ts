f6bebd22a3cf11557dbe1c79a83210761f495035
RuslanKharisov <ruslan.kharisov@gmail.com>
2023-07-06 23:00:54 +0300

    Шаблоны Tilewind и роутинг

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
+import { AboutComponent } from './components/about/about.component';
+import { PricingComponent } from './components/pricing/pricing.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home", component:HomeComponent},
+  {path:"about", component:AboutComponent},
+  {path:"pricing", component:PricingComponent},
+  {path:"login", component:LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
